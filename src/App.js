09a43d3ac5a4d31f8d769809a1b9da5147846d1a
Autoshi-k <autoshigit@gmail.com>
2021-10-31 01:34:22 -0900

    can display rules rn, kinda ugly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
+import { useState } from 'react'; 
 import Header from './components/Header/Header';
 import Game from './components/Game/Game';
 import Rules from './components/Rules/Rules';
 
+
 function App() {
+  
+  const [displayRules, setDisplayRules] = useState(false);
+  const displayRulesWindow = () => {
+    setDisplayRules(!displayRules);
+  }
+
   return (
     <div className="App">
+      { displayRules && <div className="blur-background"></div> }
       <Header />
       <Game />
-      <Rules />
+      <Rules displayRules={ displayRules } displayRulesWindow={ displayRulesWindow }/>
     </div>
   );
 }
--- src/components/Rules/Rules.js
+++ src/components/Rules/Rules.js
@@ -1,8 +1,19 @@
 import './Rules.css';
+import rulesImg from '../../images/image-rules.svg';
 
-function Rules(params) {
+function Rules({ displayRules , displayRulesWindow}) {
+
+  
   return (
-    <button className="rules" onClick="">Rules</button>
+    <>
+    <button className="rules" onClick={ displayRulesWindow }>Rules</button>
+    { displayRules && (
+      <div className="display-rules">
+        <h3>Rules</h3>
+        <img className="rules-image" src={ rulesImg } />
+        <div className="close-display" onClick={ displayRulesWindow }>X</div>
+      </div>)}
+    </>
   );
 }
 
